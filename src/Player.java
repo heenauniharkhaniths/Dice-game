98a6c98fb8c757488c3a4c5011512450e3624236
heenauniharkhaniths <HeenaUnihar.Khan@iths.se>
2025-12-24 15:21:37 +0100

    Added Player class with points and lives

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,30 @@
 public class Player {
+
+    private int points = 0;
+    private int lives = 3;
+
+    public void addPoints() {
+        points++;
+    }
+
+    public void loselive() {
+        lives--;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
+    public int getLives(){
+        return lives;
+    }
+
+    public boolean hasWon(){
+        return points >= 10;
+    }
+
+    public boolean haslives(){
+        return lives > 0;
+    }
     
 }
