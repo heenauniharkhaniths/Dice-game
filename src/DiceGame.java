a28fabd20088674d4add61181445c261c31e1bee
heenauniharkhaniths <HeenaUnihar.Khan@iths.se>
2026-01-03 11:05:52 +0100

    implemented basic game loop

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class DiceGame {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    DiceSet dice = new DiceSet();
+    Player player = new Player();
+    Scanner scan = new Scanner(System.in);
+
+    public void Start(){
+        System.out.println("Welcome to the Dice Guessing Game");
+        System.out.println("You start with 0 points and have 3 lifes" +
+        "you have to secure 10 points to win before losing 3 lives");
+
+        while (player.haslives() && !player.hasWon()) {
+            playRoundOne ();
+        }
+
+        if (player.hasWon()) {
+            System.out.println("congratulations! you won");
+        }else {
+            System.out.println("Sorry! You lost, Try again next time");
+        }
+    }
+    public static void main(String[] args) {
+        
     }
 }
